dde9d68e7cb3d10c198e71a08af4a1c5c162f6b0
KIMKICHAN <rlcks980705@gmail.com>
2022-11-02 02:13:04 +0900

    feat: boards service

---
+++ src/boards/boards.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { PatchBoardDto } from './dto/patch-board.dto';
+
+@Injectable()
+export class BoardsService {
+  private boards: Board[] = [];
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
+
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PRIVATE,
+    };
+    this.boards.push(board);
+    return board;
+  }
+
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  deleteBoard(id: string): void {
+    this.boards.filter((board) => board.id !== id);
+  }
+
+  updateBoard(id: string, patchBoardDto: PatchBoardDto): Board {
+    const board = this.getBoardById(id);
+    const { status } = patchBoardDto;
+    console.log(status);
+    board.status = status;
+    return board;
+  }
+}
