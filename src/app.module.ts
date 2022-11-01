f537f9ab8c6627e208b138876b8071b5ba08a917
KIMKICHAN <rlcks980705@gmail.com>
2022-11-02 01:01:24 +0900

    chore: clean project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
 
 @Module({
   imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
