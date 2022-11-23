772a3baf9f2a5c011d2102addba220df59c9afe3
KIMKICHAN <rlcks980705@gmail.com>
2022-11-23 23:55:50 +0900

    refactor: 모듈에서 CustomRepository import

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmExModule } from 'src/typeorm-ex.module';
 import { BoardRepository } from './board.repository';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BoardRepository])],
+  imports: [TypeOrmExModule.forCustomRepository([BoardRepository])],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
