9e82f4fa0f0758a7a6869a121a3e3eb641de8d55
KIMKICHAN <rlcks980705@gmail.com>
2022-11-23 23:55:10 +0900

    feat: CustomRepository deco

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,4 +1,6 @@
+import { CustomRepository } from 'src/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
 import { Board } from './board.entity';
 
+@CustomRepository(Board)
 export class BoardRepository extends Repository<Board> {}
